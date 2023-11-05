d816de41e726a812045222600dbcf4c04a2cc271
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-05 18:48:27 +0700

    fixing category ui in mobile

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
-
 const Categories = () => {
   return (
-    <div className="flex flex-row space-x-3  justify-center font-semibold text-base items-center w-full">
-      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out   border-transparent border-b-2 hover:border-gray-500  hover:border-b-2">
+    <div className="flex flex-wrap space-x-3 justify-center font-semibold text-base items-center w-full">
+      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
         All
       </h1>
-      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out  border-transparent border-b-2 hover:border-gray-500 ">
+      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
         Electronics
       </h1>
-      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out  border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
+      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
         Jewelry
       </h1>
-      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out  border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
+      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
         Men's Clothing
       </h1>
-      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out  border-transparent border-b-2 hover:border-gray-500 hover:border-b-2">
+      <h1 className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-gray-500 hover.border-b-2">
         Women's Clothing
       </h1>
     </div>
