068e2c5d7ec12067dd522e493f36f5f13c33167e
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-06 17:16:55 +0700

    fixing category

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
 import ProductDetail from "./pages/ProductDetail";
-import FilterPage from "./pages/FilterPage";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/products/:category" element={<FilterPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,61 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Link, useLocation } from "react-router-dom";
 
 const Categories = () => {
-  const location = useLocation();
-
   return (
     <div className="flex flex-wrap space-x-3 justify-center font-semibold text-base items-center w-full">
-      <Link
-        to="/"
-        className={`cursor-pointer py-2 transition duration-500 ease-in-out ${
-          location.pathname === "/"
-            ? "border-gray-500 border-b-2"
-            : "border-transparent"
-        }`}
-      >
-        All
-      </Link>
-      <Link
-        to="/products/electronics"
-        className={`cursor-pointer py-2 transition duration-500 ease-in-out ${
-          location.pathname === "/electronics"
-            ? "border-gray-500 border-b-2"
-            : "border-transparent"
-        }`}
-      >
-        Electronics
-      </Link>
-      <Link
-        to="/products/jewelry"
-        className={`cursor-pointer py-2 transition duration-500 ease-in-out ${
-          location.pathname === "/jewelry"
-            ? "border-gray-500 border-b-2"
-            : "border-transparent"
-        }`}
-      >
-        Jewelry
-      </Link>
-      <Link
-        to="/products/mens-clothing"
-        className={`cursor-pointer py-2 transition duration-500 ease-in-out ${
-          location.pathname === "/mens-clothing"
-            ? "border-gray-500 border-b-2"
-            : "border-transparent"
-        }`}
-      >
-        Men's Clothing
-      </Link>
-      <Link
-        to="/products/womens-clothing"
-        className={`cursor-pointer py-2 transition duration-500 ease-in-out ${
-          location.pathname === "/womens-clothing"
-            ? "border-gray-500 border-b-2"
-            : "border-transparent"
-        }`}
-      >
-        Women's Clothing
-      </Link>
+      <div className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-b-gray-400">
+        <a href="#">All</a>
+      </div>
+      <div className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-b-gray-400">
+        <a href="#">Electronics</a>
+      </div>
+      <div className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-b-gray-400">
+        <a href="#">Jewelry</a>
+      </div>
+      <div className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-b-gray-400">
+        <a href="#">Men's Clothing</a>
+      </div>
+      <div className="cursor-pointer py-2 transition duration-500 ease-in-out border-transparent border-b-2 hover:border-b-gray-400">
+        <a href="#">Women's Clothing</a>
+      </div>
     </div>
   );
 };
