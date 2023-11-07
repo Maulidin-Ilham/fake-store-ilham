e7b99caf50dc7ca3f537f78bd268522494636ac8
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-07 08:25:35 +0700

    adding cart page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import Navbar from "./components/Navbar";
-import Cart from "./pages/Cart";
+
 import ProductDetail from "./pages/ProductDetail";
 import FilterPage from "./pages/FilterPage";
 import NotFound from "./pages/NotFound";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<CartPage />} />
         <Route path="/products/:category" element={<FilterPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,9 +1,9 @@
-const Cart = () => {
+const CartPage = () => {
   return (
     <div className="grid grid-cols-1  mx-10 my-10  lg:max-w-4xl lg:mx-auto md:max-w-2xl md:mx-auto">
-      <h1 className="text-xl font-semibold">This page is under development</h1>
+      <h1 className="text-lg font-semibold">This page is under development</h1>
     </div>
   );
 };
 
-export default Cart;
+export default CartPage;
